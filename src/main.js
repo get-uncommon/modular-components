49ab73a36dbc2ad610685fea356611810c6e3ebb
Midas Minnegal <450238@student.saxion.nl>
2020-05-26 20:53:10 +0200

    Install slider modules And implement them into the main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueSVGIcon from 'vue-svgicon';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 import App from './App.vue';
 import './assets/scss/main.scss';
 
 Vue.use(VueSVGIcon);
+Vue.use(VueAwesomeSwiper);
 
 Vue.config.productionTip = false;
 
