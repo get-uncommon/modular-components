af1fda43620d0d4bcbeec1f78a23efd66670a8dd
MidasMinnegal <info@midasminnegal.nl>
2020-06-07 00:39:32 +0200

    Added SVG files

---
+++ src/icons/pause.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'pause': {
+    width: 12,
+    height: 24,
+    viewBox: '0 0 12 24',
+    data: '<path pid="0" _fill="#fff" d="M0 5h4v14H0zM8 5h4v14H8z"/>'
+  }
+})
