fb719ea4427a541543bf63aa2b03e7ab5314f421
MidasMinnegal <info@midasminnegal.nl>
2020-06-07 21:55:39 +0200

    Added contact form Based on the newsletter form with a couple of small changes.

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,198 @@
+<template>
+  <div class="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <h1 class="contact__title">
+            {{ title }}
+          </h1>
+          <p
+            v-if="body"
+            class="h5 u-margin-bottom-md"
+          >
+            {{ body }}
+          </p>
+          <form
+            class="contact__form"
+            @submit.prevent="submit"
+          >
+            <Input
+              ref="nameInput"
+              :input-props="{
+                type: 'text',
+              }"
+              :label="nameText"
+              class="u-margin-bottom-md"
+            />
+            <Input
+              ref="emailInput"
+              :input-props="{
+                type: 'text',
+                'v-model': 'email'
+              }"
+              :label="emailText"
+              class="u-margin-bottom-md"
+            />
+            <Input
+              ref="phoneInput"
+              :input-props="{
+                type: 'text',
+                'v-model': 'phone'
+              }"
+              :label="phoneText"
+              class="u-margin-bottom-md"
+            />
+            <Input
+              ref="messageInput"
+              :input-props="{
+                type: 'text',
+                'v-model': 'message'
+              }"
+              :label="messageText"
+              class="u-margin-bottom-md"
+            />
+            <Button
+              v-bind="buttonProps"
+              class="u-margin-right-md u-margin-bottom-md"
+            >
+              {{ buttonText }}
+            </Button>
+            <Message
+              v-if="success"
+              type="success"
+              :message="success"
+            />
+            <Message
+              v-for="error in errors"
+              :key="error"
+              :message="error"
+              type="error"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import emailValidator from 'email-validator';
+import Button from '@/components/Button.vue';
+import Input from '@/components/Input.vue';
+import Message from '@/components/Message.vue';
+
+export default {
+  name: 'ContactForm',
+
+  components: { Message, Input, Button },
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      default: null,
+    },
+    nameText: {
+      type: String,
+      required: true,
+    },
+    emailText: {
+      type: String,
+      required: true,
+    },
+    messageText: {
+      type: String,
+      required: true,
+    },
+    phoneText: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    buttonProps: {
+      type: Object,
+      default: () => {},
+    },
+    successText: {
+      type: String,
+      required: true,
+    },
+    failText: {
+      type: Object,
+      required: true,
+    },
+    submitHandler: {
+      type: Function,
+      default: () => null,
+    },
+  },
+
+  data() {
+    return {
+      errors: [],
+      success: null,
+    };
+  },
+
+  methods: {
+    submit() {
+      this.success = null;
+      this.errors = [];
+      this.$refs.nameInput.setError(false);
+      this.$refs.emailInput.setError(false);
+
+      const name = this.$refs.nameInput.getValue();
+      const email = this.$refs.emailInput.getValue();
+      const phone = this.$refs.phoneInput.getValue();
+      const message = this.$refs.messageInput.getValue();
+
+      const validEmail = emailValidator.validate(email);
+      if (name && email && validEmail) {
+        this.success = this.successText;
+        this.submitHandler({
+          name, email, phone, message,
+        });
+      } else {
+        if (!validEmail) {
+          this.errors.push(this.failText.email);
+          this.$refs.emailInput.setError(true);
+        }
+        if (!(name && email && message)) {
+          this.errors.push(this.failText.required);
+          this.$refs.nameInput.setError(!name);
+          this.$refs.messageInput.setError(!message);
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$offset: 89px;
+$offset-mob: 72px;
+
+.contact {
+  margin-top: $offset-mob;
+  padding-bottom: var(--spacing-xl);
+  background-color: var(--color-light);
+
+  @include media-breakpoint-up(lg) {
+    margin-top: calc(#{$offset} + var(--spacing-lg));
+  }
+
+  &__title {
+    margin-top: -#{$offset-mob};
+
+    @include media-breakpoint-up(lg) {
+      margin-top: -#{$offset};
+    }
+  }
+}
+</style>
