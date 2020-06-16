3ca5c408ad999878f2bab503427afaa5fbd25298
MidasMinnegal <info@midasminnegal.nl>
2020-06-16 22:31:44 +0200

    Added footer primary links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,94 @@
 <template>
-  <footer>
-    Dit is een footer woowowow
+  <footer class="footer">
+    <div class="container">
+      <nav
+        v-if="primaryLinks"
+        class="footer__menu footer__menu--primary"
+      >
+        <ul>
+          <li
+            v-for="link in primaryLinks"
+            :key="link.href"
+          >
+            <component
+              :is="link.as ? link.as : 'a'"
+              class="footer__menu__item h2"
+              v-bind="link.props"
+            >
+              {{ link.text }}
+            </component>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
   name: 'Footer',
+
+  props: {
+    primaryLinks: {
+      type: Array,
+      default: null,
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-footer {
+.footer {
   width: 100%;
+  padding-top: var(--spacing-xl);
+  padding-bottom: var(--spacing-lg);
+  color: var(--color-light);
   background-color: var(--color-primary);
+
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+
+  &__menu {
+    margin-top: var(--spacing-lg);
+
+    &__item {
+      display: inline-block;
+      position: relative;
+      margin-bottom: var(--spacing-md);
+      overflow: hidden;
+      color: var(--color-light);
+      text-decoration: none;
+      opacity: .5;
+      transition: $transition-base;
+
+      &::after {
+        position: absolute;
+        bottom: 4px;
+        left: 0;
+        z-index: 0;
+        width: 100%;
+        height: 2px;
+        content: '';
+        background-color: var(--color-light);
+        opacity: 0;
+        transition: $transition-base;
+        transform: translateX(-100%);
+      }
+
+      &:hover,
+      &:focus,
+      &.active {
+        outline: 0;
+        opacity: 1;
+
+        &::after {
+          opacity: 1;
+          transform: translateX(0);
+        }
+      }
+    }
+  }
 }
 </style>
