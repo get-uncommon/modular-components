cb126a4f0e1fab788fec58cc73066aab152758d8
MidasMinnegal <info@midasminnegal.nl>
2020-08-31 20:44:12 +0200

    Remove tab In order to make everything look nice.

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -77,70 +77,70 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .input {
-    position: relative;
+.input {
+  position: relative;
+  font-size: var(--font-size-input);
+
+  &__label {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    padding: var(--input-padding);
+    transition: var(--transition-base);
+    transform: translateY(-50%) scale(1);
+    -webkit-transform-origin-x: 0;
+
+    &--textarea {
+      top: 0;
+      transform: translateY(0) scale(1);
+    }
+  }
+
+  &__field {
+    width: 100%;
+    padding: var(--input-padding);
     font-size: var(--font-size-input);
+    background-color: transparent;
+    border: 0;
+    border-bottom: 1px solid var(--input-color);
+    transition: var(--transition-base);
 
-    &__label {
-      position: absolute;
-      top: 50%;
-      left: 0;
-      padding: var(--input-padding);
-      transition: var(--transition-base);
-      transform: translateY(-50%) scale(1);
-      -webkit-transform-origin-x: 0;
-
-      &--textarea {
-        top: 0;
-        transform: translateY(0) scale(1);
-      }
-    }
-
-    &__field {
+    &--label {
       width: 100%;
-      padding: var(--input-padding);
-      font-size: var(--font-size-input);
-      background-color: transparent;
-      border: 0;
-      border-bottom: 1px solid var(--input-color);
-      transition: var(--transition-base);
+    }
 
-      &--label {
-        width: 100%;
-      }
-
-      &--textarea {
-        .input__label {
-          display: none;
-        }
-      }
-
-      &:focus,
-      &.active {
-        border-bottom: 1px solid var(--input-color-active);
-        outline: 0;
-
-        + span {
-          transform: translateY(-115%) scale(.75);
-
-          &.input__label--textarea {
-            transform: translateY(-60%) scale(.75);
-          }
-        }
+    &--textarea {
+      .input__label {
+        display: none;
       }
     }
 
-    &.error {
-      .input {
-        &__field {
-          color: var(--color-danger);
-          border-bottom: 1px solid var(--color-danger);
-        }
+    &:focus,
+    &.active {
+      border-bottom: 1px solid var(--input-color-active);
+      outline: 0;
 
-        &__label {
-          color: var(--color-danger);
+      + span {
+        transform: translateY(-115%) scale(.75);
+
+        &.input__label--textarea {
+          transform: translateY(-60%) scale(.75);
         }
       }
     }
   }
+
+  &.error {
+    .input {
+      &__field {
+        color: var(--color-danger);
+        border-bottom: 1px solid var(--color-danger);
+      }
+
+      &__label {
+        color: var(--color-danger);
+      }
+    }
+  }
+}
 </style>
