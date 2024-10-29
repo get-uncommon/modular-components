7df364947faa4254a85f78a01c81181cb70aa120
Rutger Bakker <rutger7bakker@gmail.com>
2024-10-29 19:16:19 +0100

    Updates

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
+const { extract } = require("query-string");
+
 module.exports = {
   pluginOptions: {
     lintStyleOnBuild: true,
   },
   css: {
+    extract: {
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css',
+    },
     loaderOptions: {
       sass: {
         additionalData: '@import "~@/assets/scss/main.scss";',
